1a26d8c65e0863816951600ecb4fb54e01fd0a40
jisu9169 <wltn9169@naver.com>
2025-11-28 18:01:39 +0900

    naverLogin

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -176,9 +176,24 @@ const Login = () => {
             </button>
 
             {/* Naver */}
-            <button className="w-12 h-12 rounded-xl bg-[#03C75A] hover:bg-[#02B150] transition-colors flex items-center justify-center">
-              <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M5 4H9.5L14.5 12.5V4H19V20H14.5L9.5 11.5V20H5V4Z" fill="white" />
+            <button
+              className="w-12 h-12 rounded-xl bg-[#03C75A] hover:bg-[#02B150] transition-colors flex items-center justify-center"
+              onClick={() => {
+                const backendUrl = import.meta.env.VITE_API_BASE_URL;
+                window.location.href = `${backendUrl}/login/naver`;
+              }}
+            >
+              <svg
+                width="32"
+                height="32"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M5 4H9.5L14.5 12.5V4H19V20H14.5L9.5 11.5V20H5V4Z"
+                  fill="white"
+                />
               </svg>
             </button>
 
