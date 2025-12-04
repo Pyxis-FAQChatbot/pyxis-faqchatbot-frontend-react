4855a83e72e81de5a5dbfc78f0a0473f3ac935ab
harkase <kpig713@naver.com>
2025-12-04 11:00:34 +0900

    endpoint fix
    엔드포인트 수정

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "/api", // 프록시로 로드
+  baseURL: "/api/", // 프록시로 로드
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,37 +1,37 @@
 const endpoints = {
   auth: {
-    signup: "/v1/signup",
-    login: "/v1/login",
-    logout: "/v1/logout",
-    chackid: "/v1/check-id",
-    chacknick: "/v1/check-nickname",
+    signup: "v1/signup",
+    login: "v1/login",
+    logout: "v1/logout",
+    chackid: "v1/check-id",
+    chacknick: "v1/check-nickname",
   },
   my: {
-    me: "/v1/me",
-    post: '/v1/mypage/posts',
-    comment: "/v1/mypage/comments",
-    pw: "/v1/mypage/password",
-    nick: "/v1/mypage/nickname",
-    address: "/v1/mypage/address"
+    me: "v1/me",
+    post: 'v1/mypage/posts',
+    comment: "v1/mypage/comments",
+    pw: "v1/mypage/password",
+    nick: "v1/mypage/nickname",
+    address: "v1/mypage/address"
   },
   store: {
-    create: "/v1/stores",
-    edit: "/v1/mypage/stores"
+    create: "v1/stores",
+    edit: "v1/mypage/stores"
   },
   bot: {
-    create: "/v1/chatbot",
-    rooms: "/v1/chatbot/rooms",
-    msg: (id) => `/v1/chatbot/${id}/message`,
-    delete: (id) => `/v1/chatbot/${id}`,
+    create: "v1/chatbot",
+    rooms: "v1/chatbot/rooms",
+    msg: (id) => `v1/chatbot/${id}/message`,
+    delete: (id) => `v1/chatbot/${id}`,
   },
   comm: {
-    create: "/v1/community",
-    handle: (id) => `/v1/community/${id}`,
-    list: "/v1/community/posts",    
+    create: "v1/community",
+    handle: (id) => `v1/community/${id}`,
+    list: "v1/community/posts",    
   },
   comment: {
-    create: (id) => `/v1/community/${id}/comment`,
-    handle: (id,commentId) => `/v1/community/${id}/comment/${commentId}`,
+    create: (id) => `v1/community/${id}/comment`,
+    handle: (id,commentId) => `v1/community/${id}/comment/${commentId}`,
   },
   market:{
     hour: "v1/flow/hourly",
